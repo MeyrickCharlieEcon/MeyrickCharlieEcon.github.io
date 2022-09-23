9b51c66becedd6859e3536ec6f7ae21624f4a1d4
Charlie Meyrick <75495274+MeyrickCharlieEcon@users.noreply.github.com>
2022-09-23 10:02:03 +0100

    Update getMyPortfolio.js

--- js/getMyPortfolio.js
+++ js/getMyPortfolio.js
@@ -10,8 +10,8 @@ async function getMyPortfolio() {
     // Build the URL, by adding the CORE helper:
 
     var corsHelper = 'https://api.allorigins.win/raw?url=';
-    var url1 = 'https://www.google.com/finance/quote/AAPL:NASDAQ';
-    var url2 = 'https://www.google.com/finance/quote/META:NASDAQ';
+    var url1 = 'https://www.google.com/finance/quote/LLOY:LON';
+    var url2 = 'https://www.google.com/finance/quote/BTC-USD';
 
     var urlCORS1 = corsHelper + url1;
     var urlCORS2 = corsHelper + url2;
@@ -26,24 +26,24 @@ async function getMyPortfolio() {
         // console.log(html);
 
         //Stuff we need to match:
-            // <div role="heading" aria-level="1" class="zzDege">Apple Inc</div>
+            // <div role="heading" aria-level="1" class="zzDege">Lloyds Banking Group PLC</div>
             // <div class="YMlKec fxKbKc">GBX&nbsp;44.91</div>
 
         var tickerName1 = $(html1).find(".zzDege").text();
         var tickerPrice1 = $(html1).find(".YMlKec.fxKbKc").text();
 
         console.log(tickerName1);
-        // Apple Inc
+        // Lloyds Banking Group PLC
         console.log(tickerPrice1);
         // GBX 45.44
 
-        tickerName1 = tickerName1.replace("Inc", "");
-        
+        tickerName1 = tickerName1.replace("Banking Group PLC", "Bank");
+        tickerPrice1 = tickerPrice1.replace("GBX ", "");
 
         console.log(tickerName1);
-        // Apple Inc
+        // Lloyds Banking Group PLC
         console.log(tickerPrice1);
-        // VALUE
+        // GBX 45.44
 
         var tickerResult1 = tickerName1 + ": " + tickerPrice1
         console.log(tickerResult1)
@@ -64,17 +64,17 @@ async function getMyPortfolio() {
         // console.log(html);
 
         //Stuff we need to match:
-            // <div role="heading" aria-level="1" class="zzDege">Meta Platforms Inc</div>
+            // <div role="heading" aria-level="1" class="zzDege">Lloyds Banking Group PLC</div>
             // <div class="YMlKec fxKbKc">GBX&nbsp;44.91</div>
 
         var tickerName2 = $(html2).find(".zzDege").text();
         var tickerPrice2 = $(html2).find(".YMlKec.fxKbKc").text();
 
         console.log(tickerName2);
-        // Meta Platforms Inc
+        // Bitcoin (BTC / USD)
         console.log(tickerPrice2);
 
-        tickerName2 = tickerName2.replace("Meta Platforms Inc", "Facebook (Meta)");
+        tickerName2 = tickerName2.replace(" (BTC / USD)", "");
 
         var tickerResult2 = tickerName2 + ": " + tickerPrice2
         console.log(tickerResult2)
