ff6a79793acf3d27a4ba6fe0b8131ba74addbfad
Charlie Meyrick <75495274+MeyrickCharlieEcon@users.noreply.github.com>
2022-12-31 10:47:14 +0000

    Create getMyWavesFOUR.js

---
+++ js/getMyWavesFOUR.js
@@ -0,0 +1,61 @@
+
+// Functions for getting my data:
+
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////JSON////////////////////////////////////
+
+async function getMyWavesTHREE() {
+  
+    // Build the URL, by adding the CORE helper:
+
+    var corsHelper = 'https://magicseaweed.com/Penhale-Perran-Sands-Surf-Report/1324/';
+    var urlCORS = corsHelper + url;
+
+    // See what we have:
+    console.log(urlCORS);
+
+    // Get the HTML:    
+
+    $.get(urlCORS, function(html) {
+
+        // See what we got (I turn this off when it is working)
+        // console.log(html);
+
+        // The code we use in Python and BS:
+        // results = soup.find_all("span", class_="h3 font-sans-serif heavy nomargin text-white")
+
+        // Target element:  <li class="rating-text text-dark">    2-3<small>ft</small>   </li>
+
+        // Target element 2:
+                  
+        const waves = $(html).find(".rating-text.text-dark").text();
+        const swellDetails = $(html).find(".list-group-item.list-group-item-block").text();
+
+        const forecast = $(html).find(".h3.font-sans-serif.heavy.nomargin.text-white").text();
+                
+        console.log(waves);
+        console.log(forecast);
+        console.log(swellDetails)
+       
+        //Now turn the results into JSON:
+        // outputJSON = JSON.stringify(output, null, 2); 
+        // console.log(outputJSON)
+
+        //Now display the JSON on the page:
+        // document.body.innerHTML = dataJSON;
+
+        var x1 = document.getElementById("waveLocation");
+        var x2 = document.getElementById("waveHeight");
+        var x3 = document.getElementById("swellDetails");
+        
+        x1.innerText = "Perran Sands: " + waves;
+        // x2.innerText = waves;
+        x3.innerText = swellDetails
+       
+    });
+
+}
+
+/////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
